8d7e49153fcef67be43bf1404b3d073ec34780cb
Currylover992 <kcurryconnoisseur@gmail.com>
2026-03-13 14:20:20 +0100

    snälla

--- js/comments-benjamin.js
+++ js/comments-benjamin.js
@@ -1,22 +1,27 @@
 const comments = [
-  { username: "Thundery_Sad", content: "Hejsan markus", time: "3 nov, 2025 @ 11:31am", imgSrc: "img/img-profile_s/bamsepfp" },
-  { username: "Microsoft Tech Support", content: "Kan jag få vara med i league laget", time: "4 nov, 2025 @ 2:15pm", imgSrc: "img/img-profile_s/benjamincommentspfp" },
-  { username: "FEJA", content: "Adda mig Markus", time: "5 nov, 2025 @ 9:00am", imgSrc: "img/img-profile_s/filippfp" },
-  { username: "Razorcrazy", content: "Kan du ge mig A?", time: "5 nov, 2025 @ 9:00am", imgSrc: "img/img-profile_s/filippfp" },
+  { username: "Thundery_Sad", content: "Hejsan markus", time: "3 nov, 2025 @ 11:31am", imgSrc: "../img/img-profile_s/bamsepfp.jpg" },
+  { username: "Microsoft Tech Support", content: "Kan jag få vara med i league laget", time: "4 nov, 2025 @ 2:15pm", imgSrc: "../img/img-profile_s/benjamincommentspfp.jpg" },
+  { username: "FEJA", content: "Adda mig Markus", time: "5 nov, 2025 @ 9:00am", imgSrc: "../img/img-profile_s/filippfp.jpg" },
+  { username: "Razorcrazy", content: "Kan du ge mig A?", time: "5 nov, 2025 @ 9:00am", imgSrc: "../img/img-profile_s/kirbypfp (2).jpg" },
+  { username: "Curry", content: "chilla liteeee", time: "6 nov, 2025 @ 1:45pm", imgSrc: "../img/img-profile_s/markuskusin.jpg" },
 ];
 
 function createComment(username, content, time, imgSrc = "") {
+  console.log(imgSrc);
   return `
-    <div class="comments-personer">
-      <img class="commentspfp-benjamin" src="img/img-profile_s/${imgSrc}.jpg" alt="">
-      <div class="comments-content-upper-part">
-        <div class="kommer-att-runka">
-          <span class="comments-username-benjamin">${username}</span>
-          <span class="comments-content">${content}</span>
+    <div class="comments-comments">
+      <div class="comments-personer">
+        <img class="commentspfp-benjamin" src="${imgSrc}" alt="">
+        <div class="comments-content-upper-part">
+          <div class="kommer-att-runka">
+            <span class="comments-username-benjamin">${username}</span>
+            <span class="comments-content">${content}</span>
+          </div>
+          <span class="comments-time">${time}</span>
         </div>
-        <span class="comments-time">${time}</span>
       </div>
     </div>
+
   `;
 }
 
